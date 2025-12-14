759ee35f9c459848bb39bee5e842c5891c46ac6a
Nathan Saritzky <nsaritzky@Nathans-MacBook-Pro.local>
2025-12-14 14:39:25 -0800

    Project setup

---
+++ test/aoc_2025/aoc_2025_test.clj
@@ -0,0 +1,7 @@
+(ns aoc-2025.aoc-2025-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [aoc-2025.aoc-2025 :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
